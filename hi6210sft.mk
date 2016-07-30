165df06ac5e01ca2e3822404af1d9cb6e7c2864a
hak86 <hakamelassouad@gmail.com>
2016-07-30 13:03:18 +0200

    add Mali blobs to get roms booted

--- hi6210sft.mk
+++ hi6210sft.mk
@@ -2,6 +2,10 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
+# AAPT
+PRODUCT_AAPT_CONFIG := xhdpi hdpi normal
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
 # Chromium 32 Bit
 PRODUCT_COPY_FILES += \
        $(LOCAL_PATH)/rootdir/lib/libwebviewchromium.so:system/lib/libwebviewchromium.so \
@@ -14,11 +18,36 @@ PRODUCT_COPY_FILES += \
        $(LOCAL_PATH)/rootdir/lib64/libwebviewchromium_loader.so:system/lib64/libwebviewchromium_loader.so \
        $(LOCAL_PATH)/rootdir/lib64/libwebviewchromium_plat_support.so:system/lib64/libwebviewchromium_plat_support.so
 
+# Dalvik
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+
+# Display
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
 # Gatord
 PRODUCT_PACKAGES += \
 	gatord
 
 # Graphics
+PRODUCT_PROPERTY_OVERRIDES += \
+    	debug.hwui.render_dirty_regions=false \
+    	hw.lcd.lcd_density=320 \
+    	persist.sys.strictmode.disable=1 \
+    	persist.sys.use_dithering=2 \
+    	ro.opengles.version=131072 \
+    	ro.sf.lcd_density=320 \
+
+PRODUCT_COPY_FILES += \
+        $(LOCAL_PATH)/rootdir/lib/egl/libGLES_mali.so:system/lib/egl/libGLES_mali.so \
+        $(LOCAL_PATH)/rootdir/lib/hw/gralloc.hi6210sft.so:system/lib/hw/gralloc.hi6210sft.so \
+        $(LOCAL_PATH)/rootdir/lib/libion.so:system/lib/libion.so
+
+PRODUCT_COPY_FILES += \
+        $(LOCAL_PATH)/rootdir/lib64/egl/libGLES_mali.so:system/lib64/egl/libGLES_mali.so \
+        $(LOCAL_PATH)/rootdir/lib64/hw/gralloc.hi6210sft.so:system/lib64/hw/gralloc.hi6210sft.so \
+        $(LOCAL_PATH)/rootdir/lib64/libion.so:system/lib64/libion.so
+
 PRODUCT_PACKAGES += \
 	gralloc.hi6210sft \
 	libGLES_android \
