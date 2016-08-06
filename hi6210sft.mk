0c414a86281131aebb7f04c0a7fdcde307c340b2
hak86 <hakamelassouad@gmail.com>
2016-08-06 02:50:59 +0200

    Ramdisk: add one based on B190

--- hi6210sft.mk
+++ hi6210sft.mk
@@ -120,6 +120,52 @@ PRODUCT_COPY_FILES += \
         frameworks/native/data/etc/android.software.app_widgets.xml:system/etc/permissions/android.software.app_widgets.xml \
         frameworks/native/data/etc/android.software.backup.xml:system/etc/permissions/android.software.backup.xml \
 
+# Ramdisk by Haky86 based on B190 Firmware
+PRODUCT_COPY_FILES += \
+        $(LOCAL_PATH)/ramdisk/fstab.hi6210sft:root/fstab.hi6210sft \
+        $(LOCAL_PATH)/ramdisk/init:root/init \
+        $(LOCAL_PATH)/ramdisk/init.5801.rc:root/init.5801.rc \
+        $(LOCAL_PATH)/ramdisk/init.6165.rc:root/init.6165.rc \
+        $(LOCAL_PATH)/ramdisk/init.10106.rc:root/init.10106.rc \
+        $(LOCAL_PATH)/ramdisk/init.51054.rc:root/init.51054.rc \
+        $(LOCAL_PATH)/ramdisk/init.102173.rc:root/init.102173.rc \
+        $(LOCAL_PATH)/ramdisk/init.142782.rc:root/init.142782.rc \
+        $(LOCAL_PATH)/ramdisk/init.audio.rc:root/init.audio.rc \
+        $(LOCAL_PATH)/ramdisk/init.chip.usb.rc:root/init.chip.usb.rc \
+        $(LOCAL_PATH)/ramdisk/init.connectivity.bcm43xx.rc:root/init.connectivity.bcm43xx.rc \
+        $(LOCAL_PATH)/ramdisk/init.connectivity.hi110x.rc:root/init.connectivity.hi110x.rc \
+        $(LOCAL_PATH)/ramdisk/init.connectivity.rc:root/init.connectivity.rc \
+        $(LOCAL_PATH)/ramdisk/init.device.rc:root/init.device.rc \
+        $(LOCAL_PATH)/ramdisk/init.extmodem.rc:root/init.extmodem.rc \
+        $(LOCAL_PATH)/ramdisk/init.hi6210sft.rc:root/init.hi6210sft.rc \
+        $(LOCAL_PATH)/ramdisk/init.hisi.rc:root/init.hisi.rc \
+        $(LOCAL_PATH)/ramdisk/init.manufacture.rc:root/init.manufacture.rc \
+        $(LOCAL_PATH)/ramdisk/init.performance.rc:root/init.performance.rc \
+        $(LOCAL_PATH)/ramdisk/init.platform.rc:root/init.platform.rc \
+        $(LOCAL_PATH)/ramdisk/init.protocol.rc:root/init.protocol.rc \
+        $(LOCAL_PATH)/ramdisk/init.rc:root/init.rc \
+        $(LOCAL_PATH)/ramdisk/init.recovery.hi110x.rc:root/init.recovery.hi110x.rc \
+        $(LOCAL_PATH)/ramdisk/init.recovery.hi6210sft.rc:root/init.recovery.hi6210sft.rc \
+        $(LOCAL_PATH)/ramdisk/init.tee.rc:root/init.tee.rc \
+        $(LOCAL_PATH)/ramdisk/ueventd.5801.rc:root/ueventd.5801.rc \
+        $(LOCAL_PATH)/ramdisk/ueventd.6165.rc:root/ueventd.6165.rc \
+        $(LOCAL_PATH)/ramdisk/ueventd.10106.rc:root/ueventd.10106.rc \
+        $(LOCAL_PATH)/ramdisk/ueventd.51054.rc:root/ueventd.51054.rc \
+        $(LOCAL_PATH)/ramdisk/ueventd.102173.rc:root/ueventd.102173.rc \
+        $(LOCAL_PATH)/ramdisk/ueventd.142782.rc:root/ueventd.142782.rc \
+        $(LOCAL_PATH)/ramdisk/ueventd.hi6210sft.rc:root/ueventd.hi6210sft.rc \
+
+# Ramdisk/SBIN
+PRODUCT_COPY_FILES += \
+        $(LOCAL_PATH)/ramdisk/sbin/check_root:root/sbin/check_root \
+        $(LOCAL_PATH)/ramdisk/sbin/e2fsck_s:root/sbin/e2fsck_s \
+        $(LOCAL_PATH)/ramdisk/sbin/emmc_partation:root/sbin/emmc_partation \
+        $(LOCAL_PATH)/ramdisk/sbin/hdbd:root/sbin/hdbd \
+        $(LOCAL_PATH)/ramdisk/sbin/kmsgcat:root/sbin/kmsgcat \
+        $(LOCAL_PATH)/ramdisk/sbin/logctl_service:root/sbin/logctl_service \
+        $(LOCAL_PATH)/ramdisk/sbin/oeminfo_nvm_server:root/sbin/oeminfo_nvm_server \
+        $(LOCAL_PATH)/ramdisk/sbin/teecd:root/sbin/teecd \
+
 # Zygote
 PRODUCT_COPY_FILES += \
 	system/core/rootdir/init.zygote64_32.rc:root/init.zygote64_32.rc
